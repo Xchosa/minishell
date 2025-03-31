7f5e2335e849641f436470ef3f273ac58c993d6c
Paul Overbeck <poverbec@3-F-2.42heilbronn.de>
2025-03-31 12:00:58 +0200

    meta struct

--- inc/minishell.h
+++ inc/minishell.h
@@ -1,7 +1,46 @@
 
 
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <errno.h>
+# include <stdbool.h>
+
+typedef struct s_file_node
+{
+	char				*filname;
+	// holing name of redirecting file
+	int					redir_type;
+	struct s_file_node	*next;
+}	t_file_node;
+
+typedef struct s_file_list
+{
+	t_file_node	*head;
+	t_file_node	*tail;
+	ssize_t		size;
+}	t_file_list;
 
 
+typedef struct s_cmd_list
+{
+	t_cmd_node	*head;
+	t_cmd_node	*tail;
+	ssize_t		size;
+}	t_cmd_list;
+
+// singular linked list
+
+typedef struct s_cmd_node
+{
+	char		**cmd; // option and args
+	// t_type		type; // type of token
+	struct s_cmd_node *next;
+
+}	t_cmd_node;
+
+// meta struct auf die immer zugegriffen wird 
 
 
-typdef struct t_cmd_list
--- makefile
+++ makefile
@@ -7,3 +7,8 @@ CC := cc
 
 LIBFT_PATH := libft_shell
 LIBFT := $(LIBFT_PATH)/libft.a
+
+GARBAGE_PATH := garbage_collector
+
+INC_DIRS := Includes $(LIBFT_PATH) $(GARBAGE_PATH) src/executor src/parser src/parser/
+SRC_DIRS := src
