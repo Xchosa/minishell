41c809167fb7a1e9cec9dedada7c37dba85554e1
Thilo Schuller <tschulle@3-F-1.42heilbronn.de>
2025-04-30 11:03:03 +0200

    git makes me do this

--- src/executer/executer.c
+++ src/executer/executer.c
@@ -6,68 +6,13 @@
 /*   By: tschulle <tschulle@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 15:27:00 by poverbec          #+#    #+#             */
-/*   Updated: 2025/04/22 18:02:44 by tschulle         ###   ########.fr       */
+/*   Updated: 2025/04/30 11:02:00 by tschulle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/parser.h"
 #include "../../inc/minishell.h"
 #include <stdio.h>
-
-void	print_indent(t_cmd *cmds, int indent_level)
-{
-	if (cmds->token_type == PIPE)
-		indent_level--;
-	while (indent_level-- > 0)
-		printf("%s", "\t");
-}
-
-int	has_pipes(t_cmd *cmds)
-{
-	int count;
-
-	count = 0;
-	while (cmds != NULL)
-	{
-		if (cmds->token_type == PIPE)
-			count++;
-		cmds = cmds->next;
-	}
-	return (count);
-}
-
-void	print_cmd(t_cmd *cmds, int indent_level)
-{
-	int i;
-
-	i = 0;
-	print_indent(cmds, indent_level);
-	if (cmds->cmd_arg != NULL)
-	{
-		while (cmds->cmd_arg[i] != 0)
-		{
-			printf("%s ", cmds->cmd_arg[i]);
-			i++;
-		}
-	}
-	printf("\n");
-	print_indent(cmds, indent_level);
-	if (cmds->token_type != PIPE && indent_level  > 0)
-		printf("/");
-	else  
-		printf("\\");
-	printf("\n");
-}
-
-void	print_ast(t_cmd *cmds)
-{
-	int level;
-
-	level = has_pipes(cmds);
-	print_cmd(cmds, level);
-	if (cmds->next != NULL)
-		print_ast(cmds->next);
-}
 
 int	main(void)
 {
@@ -94,3 +39,57 @@ int	main(void)
 
 	print_ast(&test_list);
 }
+// void	print_indent(t_cmd *cmds, int indent_level)
+// {
+// 	if (cmds->token_type == PIPE)
+// 		indent_level--;
+// 	while (indent_level-- > 0)
+// 		printf("%s", "\t");
+// }
+
+// int	has_pipes(t_cmd *cmds)
+// {
+// 	int count;
+
+// 	count = 0;
+// 	while (cmds != NULL)
+// 	{
+// 		if (cmds->token_type == PIPE)
+// 			count++;
+// 		cmds = cmds->next;
+// 	}
+// 	return (count);
+// }
+
+// void	print_cmd(t_cmd *cmds, int indent_level)
+// {
+// 	int i;
+
+// 	i = 0;
+// 	print_indent(cmds, indent_level);
+// 	if (cmds->cmd_arg != NULL)
+// 	{
+// 		while (cmds->cmd_arg[i] != 0)
+// 		{
+// 			printf("%s ", cmds->cmd_arg[i]);
+// 			i++;
+// 		}
+// 	}
+// 	printf("\n");
+// 	print_indent(cmds, indent_level.);
+// 	if (cmds->token_type != PIPE && indent_level  > 0)
+// 		printf("/");
+// 	else  
+// 		printf("\\");
+// 	printf("\n");
+// }
+
+// void	print_ast(t_cmd *cmds)
+// {
+// 	int level;
+
+// 	level = has_pipes(cmds);
+// 	print_cmd(cmds, level);
+// 	if (cmds->next != NULL)
+// 		print_ast(cmds->next);
+// }
