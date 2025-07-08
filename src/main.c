9bb713d28e64473e9c3dfd0537f98a9324314d28
Paul Overbeck <poverbec@1-C-29.42heilbronn.de>
2025-07-08 13:29:00 +0200

    leak tokeniser

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: poverbec <poverbec@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 15:22:38 by poverbec          #+#    #+#             */
-/*   Updated: 2025/07/08 11:45:38 by poverbec         ###   ########.fr       */
+/*   Updated: 2025/07/08 13:28:24 by poverbec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,29 +20,29 @@
 //     system("leaks minishell: \n");
 // }
 
-// int	main(int argc, char **argv, char **env)
-// {
-// 	bool	tty_shell;
-// 	char	*line;
+int	main(int argc, char **argv, char **env)
+{
+	bool	tty_shell;
+	char	*line;
 
-// 	tty_shell = false;
-// 	line = NULL;
+	tty_shell = false;
+	line = NULL;
 
-// 	if (init_bash(env, argc) == false || (init_exit_codes(argc) == false))
-// 		return (1);
+	if (init_bash(env, argc) == false || (init_exit_codes(argc) == false))
+		return (1);
 
-// 	if (isatty(STDIN_FILENO))
-// 	{
-// 		interactive_shell_tty(argc, argv, env, line);
-// 		tty_shell = true;
-// 	}
-// 	else
-// 	{
-// 		non_interactive_shell(argc, argv, env, line);
-// 		{
-// 			if (tty_shell == true)
-// 			printf("clean history function + destroy shell");
-// 		}
-// 	}
-//     // destroy shell? tyname, ttyslo
-// }
+	if (isatty(STDIN_FILENO))
+	{
+		interactive_shell_tty(argc, argv, env, line);
+		tty_shell = true;
+	}
+	else
+	{
+		non_interactive_shell(argc, argv, env, line);
+		{
+			if (tty_shell == true)
+			printf("clean history function + destroy shell");
+		}
+	}
+    // destroy shell? tyname, ttyslo
+}
