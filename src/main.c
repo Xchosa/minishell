42e7296120c1477590fc2c0c631f3b95a95086d8
Paul Overbeck <poverbec@1-B-31.42heilbronn.de>
2025-07-22 16:36:37 +0200

    fixed

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: poverbec <poverbec@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 15:22:38 by poverbec          #+#    #+#             */
-/*   Updated: 2025/07/22 16:34:46 by poverbec         ###   ########.fr       */
+/*   Updated: 2025/07/22 16:35:36 by poverbec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,18 @@ int	main(int argc, char **argv, char **env)
 	(void)argv;
 	line = NULL;
 
-// 	if (init_bash(env, argc) == false || (init_exit_codes(argc) == false))
-// 		return (1);
+	if (init_bash(env, argc) == false || (init_exit_codes(argc) == false))
+		return (1);
 
-// 	if (isatty(STDIN_FILENO))
-// 	{
-// 		interactive_shell_tty(line);
-// 		rl_clear_history();
-// 	}
-// 	else
-// 		non_interactive_shell();
+	if (isatty(STDIN_FILENO))
+	{
+		interactive_shell_tty(line);
+		rl_clear_history();
+	}
+	else
+		non_interactive_shell();
 	
-// }
+}
 
 // >tmp_out | echo 1 
 // seq fault
